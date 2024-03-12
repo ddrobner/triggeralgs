10993106bd25c788ac6e273fb10efd57656c8b7f
aeoranday <alejandro@oran.day>
2024-03-12 13:45:19 +0100

    Bundle N TPs per TA.

---
+++ include/triggeralgs/BundleN/TriggerActivityMakerBundleN.hpp
@@ -0,0 +1,32 @@
+/**
+ * @file TriggerActivityMakerBundleN.hpp
+ *
+ * This is part of the DUNE DAQ Application Framework, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
+
+#ifndef TRIGGERALGS_BUNDLEN_TRIGGERACTIVITYMAKERBUNDLEN_HPP_
+#define TRIGGERALGS_BUNDLEN_TRIGGERACTIVITYMAKERBUNDLEN_HPP_
+
+#include "triggeralgs/TriggerActivityFactory.hpp"
+
+#include <vector>
+
+namespace triggeralgs {
+
+class TriggerActivityMakerBundleN : public TriggerActivityMaker
+{
+  public:
+    void operator()(const TriggerPrimitive& input_tp, std::vector<TriggerActivity>& output_tas);
+    void configure(const nlohmann::json& config);
+    bool bundle_condition();
+
+  private:
+      uint64_t m_bundle_num = 1;
+      TriggerActivity m_current_ta;
+};
+
+} // namespace triggeralgs
+
+#endif // TRIGGERALGS_BUNDLEN_TRIGGERACTIVITYMAKERBUNDLEN_HPP_
